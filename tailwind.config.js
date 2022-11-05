33c0c282077d9d7936d6bd661309465df6a88f7e
leaftech-rahad <leaftech.rahad@gmail.com>
2022-11-05 14:53:35 +0600

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navbar: "#161B22",
+      },
+    },
   },
   plugins: [],
 };
